cae93d3570405a579c67e48244279f4a22454d17
Ryoter <53593515+RyoyaMizukami@users.noreply.github.com>
2019-10-16 10:25:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('McDonald.html')
+    return render_template('.html')
 
 if __name__ == "__main__":
     app.run()
