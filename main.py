f8900309d0882e5005e3632d448f2cc34abe46eb
RyoyaMizukami <s18102@kaetsu.ac.jp>
2019-10-25 21:24:58 +0900

    sprint4 終わり

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ def get_connection():
     dsn = os.environ.get('DATABASE_URL')
     return psycopg2.connect(dsn)
 
-cur = get_connection().cursor()
-cur.execute('SELECT * FROM zgundam ORDER BY id ASC')
-data = cur.fetchall()
-cur.close()
-get_connection().close()
-
 @app.route('/')
 def index():
-    return render_template('index.html', len = len(data), data = data)
+    return render_template('index.html', len = 0, data = [])
 
 @app.route('/search')
 def search():
     return render_template('commons/search.html')
 
 @app.route('/post', methods=['GET', 'POST'])
-def post():    
+def post():
+    IsValue = False
     if request.method == 'POST':
        budget = request.form['budget']
-       if budget != None:
-         return redirect(url_for('index'))
-       return redirect(url_for('search'))
+       print(budget)
+       if not budget:
+         IsValue = True
+         return render_template('commons/search.html', IsValue = IsValue)
+       cur = get_connection().cursor()
+       cur.execute('SELECT * FROM zgundam WHERE money <= %s ORDER BY money ASC', (budget,))
+       data = cur.fetchall()
+       cur.close()
+       get_connection().close()
+       return render_template('index.html', budget = budget, data = data, len = len(data))
 
 if __name__ == "__main__":
     app.run()
